dd44010f475d6ba21de72f681057152e7ee70f85
Rodrigo <rodrigolp0521@gmail.com>
2025-12-19 17:14:54 -0600

    feat: update app to include ApiProvider and routes

--- dev_environment/src/public/components/app.tsx
+++ dev_environment/src/public/components/app.tsx
@@ -1,25 +1,12 @@
-import React, { useState } from "react";
-import { i18n } from "@osd/i18n";
-import { FormattedMessage, I18nProvider } from "@osd/i18n/react";
-import { BrowserRouter as Router } from "react-router-dom";
-
-import {
-  EuiButton,
-  EuiHorizontalRule,
-  EuiPage,
-  EuiPageBody,
-  EuiPageContent,
-  EuiPageContentBody,
-  EuiPageContentHeader,
-  EuiPageHeader,
-  EuiTitle,
-  EuiText,
-} from "@elastic/eui";
-
+import { I18nProvider } from "@osd/i18n/react";
+import React from "react";
+import { Route, BrowserRouter as Router } from "react-router-dom";
 import { CoreStart } from "../../../../src/core/public";
 import { NavigationPublicPluginStart } from "../../../../src/plugins/navigation/public";
-
-import { PLUGIN_ID, PLUGIN_NAME } from "../../common";
+import { MainLayout } from "../layouts/MainLayout";
+import { ApiProvider } from "../context/ApiContext";
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from "../lib/queryClient";
 
 interface CustomPluginAppDeps {
   basename: string;
@@ -34,85 +21,17 @@ export const CustomPluginApp = ({
   http,
   navigation,
 }: CustomPluginAppDeps) => {
-  // Use React hooks to manage state.
-  const [timestamp, setTimestamp] = useState<string | undefined>();
-
-  const onClickHandler = () => {
-    // Use the core http service to make a response to the server API.
-    http.get("/api/custom_plugin/example").then((res) => {
-      setTimestamp(res.time);
-      // Use the core notifications service to display a success message.
-      notifications.toasts.addSuccess(
-        i18n.translate("customPlugin.dataUpdated", {
-          defaultMessage: "Data updated",
-        })
-      );
-    });
-  };
-
-  // Render the application DOM.
-  // Note that `navigation.ui.TopNavMenu` is a stateful component exported on the `navigation` plugin's start contract.
   return (
     <Router basename={basename}>
       <I18nProvider>
-        <>
-          <navigation.ui.TopNavMenu
-            appName={PLUGIN_ID}
-            showSearchBar={true}
-            useDefaultBehaviors={true}
-          />
-          <EuiPage restrictWidth="1000px">
-            <EuiPageBody component="main">
-              <EuiPageHeader>
-                <EuiTitle size="l">
-                  <h1>
-                    <FormattedMessage
-                      id="customPlugin.helloWorldText"
-                      defaultMessage="{name}"
-                      values={{ name: PLUGIN_NAME }}
-                    />
-                  </h1>
-                </EuiTitle>
-              </EuiPageHeader>
-              <EuiPageContent>
-                <EuiPageContentHeader>
-                  <EuiTitle>
-                    <h2>
-                      <FormattedMessage
-                        id="customPlugin.congratulationsTitle"
-                        defaultMessage="Congratulations, you have successfully created a new OpenSearch Dashboards Plugin!"
-                      />
-                    </h2>
-                  </EuiTitle>
-                </EuiPageContentHeader>
-                <EuiPageContentBody>
-                  <EuiText>
-                    <p>
-                      <FormattedMessage
-                        id="customPlugin.content"
-                        defaultMessage="Look througshfdfad the generated code and check out the plugin development documentation."
-                      />
-                    </p>
-                    <EuiHorizontalRule />
-                    <p>
-                      <FormattedMessage
-                        id="customPlugin.timestampText"
-                        defaultMessage="Last timestamp: {time}"
-                        values={{ time: timestamp ? timestamp : "Unknown" }}
-                      />
-                    </p>
-                    <EuiButton type="primary" size="s" onClick={onClickHandler}>
-                      <FormattedMessage
-                        id="customPlugin.buttonText"
-                        defaultMessage="Get data"
-                      />
-                    </EuiButton>
-                  </EuiText>
-                </EuiPageContentBody>
-              </EuiPageContent>
-            </EuiPageBody>
-          </EuiPage>
-        </>
+        <QueryClientProvider client={queryClient}>
+          <ApiProvider http={http}>
+            <MainLayout>
+              <Route exact path='/' component={<></>} />
+              <Route path='/metrics' component={<></>} />
+            </MainLayout>
+          </ApiProvider>
+        </QueryClientProvider>
       </I18nProvider>
     </Router>
   );

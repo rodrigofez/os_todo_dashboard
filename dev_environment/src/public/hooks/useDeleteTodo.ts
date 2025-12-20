9a6fcae25d71dc5f6af61f4b99e7a3c861d484f7
Rodrigo <rodrigolp0521@gmail.com>
2025-12-20 13:44:59 -0600

    feat: add useDeleteTodo hook

---
+++ dev_environment/src/public/hooks/useDeleteTodo.ts
@@ -0,0 +1,31 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { API_BASE_PATH, PaginatedResponse, Todo } from "../../common";
+import { useApi } from "../context/ApiContext";
+
+export function useDeleteTodo() {
+  const { http } = useApi();
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    onMutate: (id: string) => {
+      // Optimistically delete task
+      queryClient.setQueryData(['todos'], (oldData: PaginatedResponse<Todo> | undefined) => {
+        if (oldData) return {
+          data: oldData.data.filter((todo) => todo.id !== id),
+          pagination: {
+            ...oldData.pagination,
+            total: oldData.pagination.total - 1
+          }
+        }
+
+        return oldData;
+      })
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['todos'] })
+    },
+    mutationFn: async (id: string) => http.delete<PaginatedResponse<Todo>>(
+      `${API_BASE_PATH}/todos/${id}`
+    )
+  });
+}

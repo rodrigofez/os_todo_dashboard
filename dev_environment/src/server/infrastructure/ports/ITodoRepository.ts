4134de73f517166a20d9e0f3591d3c12734bd420
Rodrigo <rodrigolp0521@gmail.com>
2025-12-19 18:11:26 -0600

    feat: add todo repository

---
+++ dev_environment/src/server/infrastructure/ports/ITodoRepository.ts
@@ -0,0 +1,33 @@
+import { Todo, PaginatedResponse } from "../../../common";
+import { TodoEntity } from "../../domain/entities/Todo";
+
+export interface TodoFilters {
+  q?: string;
+  tags?: string[];
+  status?: string;
+  priority?: string;
+}
+
+export interface PaginationParams {
+  page: number;
+  pageSize: number;
+  sortBy?: string;
+  sortOrder?: "asc" | "desc";
+}
+
+export interface SearchQuery {
+  q?: string;
+  tags?: string[];
+  status?: string[];
+  priority?: string[];
+  pagination?: PaginationParams;
+}
+
+export interface ITodoRepository {
+  save(todo: TodoEntity): Promise<void>;
+  update(id: string, todo: TodoEntity): Promise<void>;
+  findById(id: string): Promise<TodoEntity | null>;
+  delete(id: string): Promise<void>;
+  search(query: SearchQuery): Promise<PaginatedResponse<Todo>>;
+  exists(id: string): Promise<boolean>;
+}

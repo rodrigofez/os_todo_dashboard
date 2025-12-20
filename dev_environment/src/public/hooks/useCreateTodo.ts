204317e7a8458ac273c24b5a68ef30101d6cc72f
Rodrigo <rodrigolp0521@gmail.com>
2025-12-20 13:12:07 -0600

    feat: add useCreateTodo hook

---
+++ dev_environment/src/public/hooks/useCreateTodo.ts
@@ -0,0 +1,19 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { API_BASE_PATH, CreateTodoRequest, Todo } from "../../common";
+import { useApi } from "../context/ApiContext";
+
+export function useCreateTodo({ onSuccess }: { onSuccess: () => void }) {
+  const { http } = useApi();
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (data: CreateTodoRequest) => http.post<Todo>(`${API_BASE_PATH}/todos`, {
+      body: JSON.stringify(data),
+    }),
+    onSettled: () => {
+      queryClient.invalidateQueries({ queryKey: ['todos'] });
+    },
+    onSuccess
+  })
+}
+
